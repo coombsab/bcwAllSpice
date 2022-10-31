760d9a2866375bd04844206d90f1b13ab4389016
Abraham Coombs <coombsab@gmail.com>
2022-10-31 12:28:45 -0600

    some requirements for backend model

--- bcwAllSpice/Globals.cs
+++ bcwAllSpice/Globals.cs
@@ -2,6 +2,7 @@ global using System;
 global using System.Linq;
 global using System.Data;
 global using System.Collections.Generic;
+global using System.ComponentModel.DataAnnotations;
 
 global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Mvc;
--- bcwAllSpice/Models/Recipe.cs
+++ bcwAllSpice/Models/Recipe.cs
@@ -4,12 +4,17 @@ public class Recipe : IRepoItem<int> {
   public int Id { get; set; }
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
+  [Required]
   public string Title { get; set; }
   public string Subtitle { get; set; }
   public string Instructions { get; set; }
   public string Img { get; set; }
+  [Required]
   public string Category { get; set; }
+  [Required]
   public string CreatorId { get; set; }
+
+  // populated info
   public Profile Creator { get; set; }
   public List<string> FavoriteeIds { get; set; }
   public List<Profile> Favoritees { get; set; }
